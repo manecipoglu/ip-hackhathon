8c02bfd7fc6c22d6028d99a6b61d27c97e36cd6d
Muammer Necipoglu <muammernecipoglu@gmail.com>
2022-01-13 14:25:33 -0800

    Add mock data and router routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const instructorsRouter = require("./routes/instructors");
+const gymsRouter = require("./routes/gyms");
 
 const app = express();
 
@@ -8,6 +10,8 @@ const { PORT, CLIENT_URL } = process.env;
 
 app.use(cors({ origin: CLIENT_URL || true }));
 app.use(express.json());
+app.use("/instructors", instructorsRouter);
+app.use("/gyms", gymsRouter);
 
 app.listen(PORT, () =>
   console.log(`🚀🚀 Server is live @http://localhost:${PORT || 8080}`)
---
+++ server/routes/gyms.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const { readFile, writeFile } = require("../utils/helpers");
+
+const gymRouter = Router();
+const FILE_PATH = "./data/gyms.json";
+
+//Get all gyms
+gymRouter.get("/", (req, res) => {
+  const gymList = readFile(FILE_PATH);
+
+  return res.status(200).json(gymList);
+});
+
+//Get a single gym by id
+gymRouter.get("/:id", (req, res) => {
+  const gyms = readFile(FILE_PATH);
+  const gym = gyms.find((gym) => gym.id === req.params.id);
+  if (!gym) return res.status(404).send("The gym is not found");
+  res.status(200).json(gym);
+});
+
+module.exports = gymRouter;
