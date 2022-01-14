1fcb6928725d4b42422d2e50b9e02727846f1f0f
Heba <h_altabba@hotmail.com>
2022-01-14 01:38:51 -0800

    instructor card

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Header from "./components/Header/Header";
 import "./styles/App.scss";
 import OurClasses from './components/OurClasses/OurClasses';
+import OurInstructors from './components/OurInstructors/OurInstructors';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <OurClasses />
+      <OurInstructors />
     </BrowserRouter>
   );
 
