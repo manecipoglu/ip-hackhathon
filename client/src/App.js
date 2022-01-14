b372f009cb3e6b215fbcca68f52507d011b9699a
Heba <h_altabba@hotmail.com>
2022-01-13 23:33:27 -0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import "./styles/App.scss";
 import OurClasses from './components/OurClasses/OurClasses';
-import ClassCard from "./components/ClassCard/ClassCard";
 
 export default function App() {
   return <div>
